37a76a179e1ca8e5c148f0f46b903fb565b846e0
sortofasian <charliesyvertsen06@icloud.com>
2022-07-06 00:34:50 -0400

    Disable react/react-in-jsx-scope

--- admin-frontend/src/App.tsx
+++ admin-frontend/src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
 
 import Page from './Pages/Page'
 
 const Root = styled.div`
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu',
-        'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell',
+        'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
     font-style: normal;
     font-weight: 400;
     font-size: 13px;
