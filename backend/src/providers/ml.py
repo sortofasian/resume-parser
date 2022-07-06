235f8e8994fd4c65dae13bd1ceba68e3ac610050
sortofasian <charliesyvertsen06@icloud.com>
2022-07-06 00:21:48 -0400

    Set up deploy for frontends

---
+++ backend/src/providers/ml.py
@@ -0,0 +1,4 @@
+from gensim.models import Phrases, Word2Vec
+
+phrases = Phrases()  # Phrases.load("ml/phrases/phrases.model")
+word2vec = Word2Vec()  # .load("ml/word2vec/word2vec.model")
--- backend/src/resumes/model.py
+++ backend/src/resumes/model.py
@@ -1,5 +1,10 @@
-from typing import IO
+import unicodedata as uni
+from base64 import b64decode
 
+import textract
+from gensim.parsing import preprocessing as prep
+from nanoid import generate as nanoid
+from src.providers.ml import phrases, word2vec
 from src.providers.mongodb import db
 
 resumes = db["resumes"]
@@ -11,28 +16,45 @@ class Resumes:
     def __init__(self, uid):
         self.uid = uid
 
+    def parse(self, filename: str, b64file: str):
+        tempname = "temp_" + nanoid(5) + filename
+        with open(tempname) as f:
+            f.write(b64decode(b64file))
+        text = textract.process(tempname)
+        temp = uni.normalize("NFKD", text).encode("ascii", "ignore")
+        temp = temp.lower()
+        temp = temp.split(". ")
+        resume = []
+        for sentence in temp:
+            sentence = prep.preprocess_string(
+                sentence,
+                filters=[
+                    prep.remove_stopwords,
+                    prep.strip_punctuation,
+                    prep.strip_multiple_whitespaces,
+                    prep.strip_numeric,
+                ],
+            )
+            resume.append(phrases[sentence])
+        for sentence in resume:
+            for word in sentence:
+                skills = ["engineering"]
+                word2vec.wv.cosine_similarities(word, skills)
+
     def add(self, resume):
         resumes.update_one({"uid": self.uid}, {}, upsert=True)
 
-    def list(self):
-        resumes.find({"uid": self.uid})
+    def update(self, rid: str):
+        pass
 
     def get(self, rid: str):
         pass
 
-    def update(self, rid: str):
+    def get_all(self):
         pass
 
     def delete(self, rid: str):
         pass
 
-
-class Resume:
-    def __init__(self, rid):
-        self.rid = rid
-
-    def parse(file: IO):
-        pass
-
-    def update(self, update: dict):
+    def delete_all(self):
         pass
