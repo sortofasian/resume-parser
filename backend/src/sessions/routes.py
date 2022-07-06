06da57ba4b6e484e1dcee21f235e959a83056dd8
sortofasian <charliesyvertsen06@icloud.com>
2022-07-06 00:12:53 -0400

    Begin adding flask-login to handle sessions

--- backend/src/sessions/routes.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-import controller
-
-session_routes = Blueprint("sessions", __name__)
-
-session_routes.route("/create", methods=["POST"])(controller.create)
-session_routes.route("/<string:uid>", methods=["GET"])(controller.read)
-session_routes.route("/<string:uid>/logout", methods=["DELETE"])(controller.delete)
